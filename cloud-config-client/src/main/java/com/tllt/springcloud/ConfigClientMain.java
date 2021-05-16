0a60d1d6f4e415d2c4379f6f486489a55766dfd5
tllt <365121267@qq.com>
2021-05-16 21:23:28 +0800

    提交 SpringCloud Config

---
+++ cloud-config-client/src/main/java/com/tllt/springcloud/ConfigClientMain.java
@@ -0,0 +1,18 @@
+package com.tllt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author tllt
+ * @create 2021/5/16
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain.class);
+    }
+}
