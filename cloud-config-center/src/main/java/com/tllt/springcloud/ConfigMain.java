91d1501dddf012817f5db9745dd6f9aefb4b78bb
tllt_1 <365121267@qq.com>
2021-09-21 16:32:39 +0800

    提交 SpringCloud gateway

--- cloud-config-center/src/main/java/com/tllt/springcloud/ConfigMain.java
+++ cloud-config-center/src/main/java/com/tllt/springcloud/ConfigMain.java
@@ -3,6 +3,7 @@ package com.tllt.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author tllt
@@ -10,6 +11,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigMain {
     public static void main(String[] args) {
         SpringApplication.run(ConfigMain.class,args);
--- cloud-config-client/src/main/java/com/tllt/springcloud/controller/ConfigClientController.java
+++ cloud-config-client/src/main/java/com/tllt/springcloud/controller/ConfigClientController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RefreshScope
 public class ConfigClientController {
-    @Value("${config.info}")
+    @Value("${spring.application.name}")
     private String configInfo;
 
     @GetMapping("/configInfo")
